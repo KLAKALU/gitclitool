7e1cef218078d78a3b915a6c38ed06d724bb7806
KLAKALU <player4797462226@gmail.com>
2023-11-30 15:13:53 +0900

    del: genelated comments

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gitclitool",
 	Short: "A brief description of your application",
@@ -24,8 +23,6 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
 		config, _ := cmd.Flags().GetBool("config")
 		isShowMsgTrue, _ := cmd.Flags().GetBool("showmsg")
@@ -119,8 +116,6 @@ to quickly create a Cobra application.`,
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -129,14 +124,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gitclitool.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.PersistentFlags().BoolP("config", "c", false, "congiure")
 	rootCmd.PersistentFlags().BoolP("showmsg", "s", false, "show message")
