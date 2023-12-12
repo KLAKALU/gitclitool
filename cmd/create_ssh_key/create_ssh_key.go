0cc731ccebb907cd32b3d0d08403767bea109452
KLAKALU <player4797462226@gmail.com>
2023-12-12 19:22:37 +0900

    add: createsshkey file

---
+++ cmd/create_ssh_key/create_ssh_key.go
@@ -0,0 +1,132 @@
+package create_ssh_key
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"github.com/manifoldco/promptui"
+)
+
+func CreateSshKey(isShowMsgTrue bool) {
+	const DIST_DIR = ".ssh"
+
+	const SSH_KEY_NAME = "id_rsa.pub"
+
+	const OS_TYPE = runtime.GOOS
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// check ssh directory exist
+	if _, err := os.Stat(filepath.Join(homeDir, DIST_DIR)); os.IsNotExist(err) {
+		// ~/.ssh directory not exist
+		if err := os.Mkdir(filepath.Join(homeDir, DIST_DIR), 0755); err != nil {
+			fmt.Println("ssh-key directory create error")
+			os.Exit(1)
+		}
+		fmt.Println("ssh-key directory created")
+	} else {
+		if isShowMsgTrue {
+			fmt.Println("ssh-key directory already exist")
+		}
+	}
+
+	// check ssh-key exist
+	if _, err := os.Stat(filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)); os.IsNotExist(err) {
+		// ~/.ssh/id_rsa file not exist
+		out, err := exec.Command("ssh-keygen", "-t", "ed25519", "-N", "", "-f", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
+		fmt.Printf("\nssh-keygen result: %s", string(out))
+		if err != nil {
+			fmt.Println("ssh-keygen error")
+			os.Exit(1)
+		}
+		fmt.Println("ssh-keygen success")
+	} else {
+		fmt.Println("ssh-key already exist")
+	}
+	prompt := promptui.Select{
+		Label: "Copy ssh-key to clipboard?",
+		Items: []string{"yes", "no"},
+	}
+	_, out, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+	fmt.Printf("You choose %s\n", out)
+	if out == "no" {
+		os.Exit(1)
+	}
+
+	//get ssh-key value
+	var sshKey []byte
+	switch OS_TYPE {
+	case "darwin":
+		//mac
+		var err error
+		sshKey, err = exec.Command("cat", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
+		if err != nil {
+			fmt.Println("ssh-key copy error")
+			os.Exit(1)
+		}
+	case "linux":
+		//linux
+		var err error
+		sshKey, err = exec.Command("cat", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
+		if err != nil {
+			fmt.Println("ssh-key copy error")
+			os.Exit(1)
+		}
+	case "windows":
+		//windows
+		var err error
+		sshKey, err = exec.Command("powershell", "cat", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
+		if err != nil {
+			fmt.Println("ssh-key copy error")
+			os.Exit(1)
+		}
+	default:
+		//その他
+		fmt.Print("sorry, this os is not supported yet")
+		os.Exit(1)
+	}
+	fmt.Println("ssh-key get success")
+
+	//copy ssh-key to clipboard
+	switch OS_TYPE {
+	case "darwin":
+		//mac
+		_, err := exec.Command("osascript", "-e", "set the clipboard to \""+string(sshKey)+"\"").CombinedOutput()
+		if err != nil {
+			fmt.Println("ssh-key copy error")
+			os.Exit(1)
+		}
+	case "linux":
+		//linux
+		/*
+			_, err := exec.Command("xclip", "-selection", "c", "-i").CombinedOutput()
+			if err != nil {
+				fmt.Println("ssh-key copy error")
+				os.Exit(1)
+			}
+		*/
+	case "windows":
+		//windows
+		_, err := exec.Command("powershell", "\"", string(sshKey), "\"", "|", "clip").CombinedOutput()
+		if err != nil {
+			fmt.Println("ssh-key copy error")
+			os.Exit(1)
+		}
+	default:
+		fmt.Print("sorry, this os is not supported yet")
+		os.Exit(1)
+		//その他
+	}
+	fmt.Println("ssh-key copy success")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -6,12 +6,9 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
 
+	"github.com/KLAKALU/gitclitool/cmd/create_ssh_key"
 	"github.com/manifoldco/promptui"
-
 	"github.com/spf13/cobra"
 )
 
@@ -25,16 +22,12 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		const DIST_DIR = ".ssh"
-
-		const SSH_KEY_NAME = "id_rsa.pub"
-
-		const OS_TYPE = runtime.GOOS
-
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		config, _ := cmd.Flags().GetBool("config")
+		isShowMsgTrue, _ := cmd.Flags().GetBool("showmsg")
+		if config {
+			fmt.Println("config is true")
+		} else {
+			fmt.Println("config is false")
 		}
 
 		// ask what to do
@@ -48,121 +41,8 @@ to quickly create a Cobra application.`,
 			return
 		}
 		fmt.Printf("%s\n", out)
-		config, _ := cmd.Flags().GetBool("config")
-		isShowMsgTrue, _ := cmd.Flags().GetBool("showmsg")
-		if config {
-			fmt.Println("config is true")
-		} else {
-			fmt.Println("config is false")
 
-		}
-
-		// check ssh directory exist
-		if _, err := os.Stat(filepath.Join(homeDir, DIST_DIR)); os.IsNotExist(err) {
-			// ~/.ssh directory not exist
-			if err := os.Mkdir(filepath.Join(homeDir, DIST_DIR), 0755); err != nil {
-				fmt.Println("ssh-key directory create error")
-				os.Exit(1)
-			}
-			fmt.Println("ssh-key directory created")
-		} else {
-			if isShowMsgTrue {
-				fmt.Println("ssh-key directory already exist")
-			}
-		}
-
-		// check ssh-key exist
-		if _, err := os.Stat(filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)); os.IsNotExist(err) {
-			// ~/.ssh/id_rsa file not exist
-			out, err := exec.Command("ssh-keygen", "-t", "ed25519", "-N", "", "-f", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
-			fmt.Printf("\nssh-keygen result: %s", string(out))
-			if err != nil {
-				fmt.Println("ssh-keygen error")
-				os.Exit(1)
-			}
-			fmt.Println("ssh-keygen success")
-		} else {
-			fmt.Println("ssh-key already exist")
-		}
-		prompt2 := promptui.Select{
-			Label: "Copy ssh-key to clipboard?",
-			Items: []string{"yes", "no"},
-		}
-		_, out, err = prompt2.Run()
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			return
-		}
-		fmt.Printf("You choose %s\n", out)
-		if out == "no" {
-			os.Exit(1)
-		}
-
-		//get ssh-key value
-		var sshKey []byte
-		switch OS_TYPE {
-		case "darwin":
-			//mac
-			var err error
-			sshKey, err = exec.Command("cat", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
-			if err != nil {
-				fmt.Println("ssh-key copy error")
-				os.Exit(1)
-			}
-		case "linux":
-			//linux
-			var err error
-			sshKey, err = exec.Command("cat", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
-			if err != nil {
-				fmt.Println("ssh-key copy error")
-				os.Exit(1)
-			}
-		case "windows":
-			//windows
-			var err error
-			sshKey, err = exec.Command("powershell", "cat", filepath.Join(homeDir, DIST_DIR, SSH_KEY_NAME)).CombinedOutput()
-			if err != nil {
-				fmt.Println("ssh-key copy error")
-				os.Exit(1)
-			}
-		default:
-			//その他
-			fmt.Print("sorry, this os is not supported yet")
-			os.Exit(1)
-		}
-		fmt.Println("ssh-key get success")
-
-		//copy ssh-key to clipboard
-		switch OS_TYPE {
-		case "darwin":
-			//mac
-			_, err := exec.Command("osascript", "-e", "set the clipboard to \""+string(sshKey)+"\"").CombinedOutput()
-			if err != nil {
-				fmt.Println("ssh-key copy error")
-				os.Exit(1)
-			}
-		case "linux":
-			//linux
-			/*
-				_, err := exec.Command("xclip", "-selection", "c", "-i").CombinedOutput()
-				if err != nil {
-					fmt.Println("ssh-key copy error")
-					os.Exit(1)
-				}
-			*/
-		case "windows":
-			//windows
-			_, err := exec.Command("powershell", "\"", string(sshKey), "\"", "|", "clip").CombinedOutput()
-			if err != nil {
-				fmt.Println("ssh-key copy error")
-				os.Exit(1)
-			}
-		default:
-			fmt.Print("sorry, this os is not supported yet")
-			os.Exit(1)
-			//その他
-		}
-		fmt.Println("ssh-key copy success")
+		create_ssh_key.CreateSshKey(isShowMsgTrue)
 	},
 }
 
